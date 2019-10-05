5070bb16ed2d49ff687c65f762e004234956acdd
zhaojiaershao <zhaoruibo123>
2019-10-05 18:34:39 +0800

    hai

---
+++ build/precache-manifest.1ac1de4442fbb6339b5fe6dc6fa0ac63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7acca735a4e508279af0e2729696de0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3a757a73c65326dc797",
+    "url": "/static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "e654951a0acbff98086c",
+    "url": "/static/css/main.d4238b19.chunk.css"
+  },
+  {
+    "revision": "b3a757a73c65326dc797",
+    "url": "/static/js/2.4c0af039.chunk.js"
+  },
+  {
+    "revision": "e654951a0acbff98086c",
+    "url": "/static/js/main.cbaf1a7b.chunk.js"
+  },
+  {
+    "revision": "234c9a94223d125e01ff",
+    "url": "/static/js/runtime-main.4a96f6bf.js"
+  }
+]);
